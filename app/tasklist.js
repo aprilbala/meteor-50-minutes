ad44e9fe609bd2062c68dc97a362a58404bc48ed
April-Angela Bala <aaibala@hawaii.edu>
2017-10-07 22:19:22 -1000

    Task 4: Tasklist authentication

--- app/tasklist.js
+++ app/tasklist.js
@@ -11,10 +11,7 @@ if (Meteor.isClient) {
       "submit .add-task": function(event){
         let name = event.target.name.value;
 
-        Tasks.insert({
-          name: name,
-          createdAt: new Date()
-        });
+        Meteor.call('addTask', name);
 
         event.target.name.value = '';
 
@@ -22,11 +19,11 @@ if (Meteor.isClient) {
       },
 
     "click .delete-task": function(event) {
-        if (confirm('Delete Task?')) {
-          Tasks.remove(this._id);
-        }
+      if (confirm('Delete Task?')) {
+        Meteor.call('deleteTask', this._id);
+      }
 
-        return false;
+      return false;
     }
   })
 }
@@ -34,3 +31,22 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
 }
+
+Meteor.methods({
+  addTask: function(name) {
+    if(!Meteor.userId()) {
+      throw new Meteor.Error('No Access!');
+    }
+
+    Tasks.insert({
+      name: name,
+      createdAt: new Date(),
+      userId: Meteor.userId()
+    });
+  },
+
+  deleteTask: function(taskId) {
+    Tasks.remove(taskId);
+  }
+
+});
